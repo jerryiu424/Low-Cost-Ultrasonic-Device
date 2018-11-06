a9462350c38a75373f17cb3cf1aeccb0461e3c38
Morgan Patrick O'Brien <mobrie69@uwo.ca>
2018-11-06 14:51:12 -0500

    commit3

--- LCUD_Project/Sensor.cpp
+++ LCUD_Project/Sensor.cpp
@@ -8,9 +8,14 @@
 using namespace std;
 int main() {
 
+  //To reid or vance or whomever:  This code shows you how the pi can take in the values from the arduino and output them.
+  //This should not be in the main like this, it's just a proof of concept.
+  //feel free to completely delete it once you have it in the right class
+
 	ifstream inFile;
+	string sensorID = 0;
 	string line;
-	inFile.open("/dev/ttyACM2");
+	inFile.open("/dev/ttyACM" + sensorID);
 	while(getline(inFile, line))
 	{
 
@@ -25,7 +30,7 @@ int main() {
 	cout << "\nDuration: " << duration;
 	cout << "\nTemperature: " << temperature;
 }
-	usleep(100000);
+	usleep(100000);//pause for 1 second
 
 
 }	
