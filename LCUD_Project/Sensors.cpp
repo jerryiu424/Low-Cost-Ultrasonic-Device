e46f79d2e1444f077d0428bd8b542b91780e48c2
Morgan Patrick O'Brien <mobrie69@uwo.ca>
2018-11-07 15:30:47 -0500

    updated sensor

---
+++ LCUD_Project/Sensors.cpp
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <fstream>
+#include <string>
+#include <dirent.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include "Sensors.h"
+
+Sensors::Sensors(){
+    std::ifstream inFile;
+    //get number of current sensors, and increment total number of sensors by 1
+	int ID = NumSensors;
+    NumSensors++;
+
+    SensorID = std::to_string(ID);    //convert int to string
+	inFile.open("/dev/ttyACM" + SensorID);    //open file where sensor is located
+    //while there is data, keep reading
+    std::string line;    
+	while(getline(inFile, line))
+	{
+        std::stringstream ss(line);
+        std::string duration;
+        getline(ss,duration, ' ');
+        std::string temperature;
+        getline(ss,temperature,' ');
+
+        //if there is data, set attributes accordingly
+        if(!duration.empty() && !temperature.empty()){
+            SDuration = duration;
+            STemperature = temperature;
+        }
+
+	usleep(100000);//pause for 1 second
+    }	
+	inFile.close();
+}
+
+std::string Sensors::to_string(){
+    return "Sensor number "+SensorID+" currently has duration "+SDuration+" and temperature "+STemperature;
+}
---
+++ LCUD_Project/Sensors.h
@@ -0,0 +1,21 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+#include <string>
+
+class Sensors{
+    private:
+        std::string SensorID,SDuration,STemperature;    //attributes ID, duration, and temperature
+        static int NumSensors;    //static class varible NumSensors that keeps track of the total number of sensors 
+    public:
+        //constructor, destructor, getters, and toString methods
+        Sensors();
+        ~Sensors();
+        int getNumberOfSensors(){return NumSensors;}
+        std::string getIdentification(){return SensorID;}
+        std::string getDuration(){return SDuration;}
+        std::string getTemperature(){return STemperature;}
+        std::string to_string();
+};
+//initialize number of sensors NumSensors to 0
+int Sensors::NumSensors = 0;
+#endif
