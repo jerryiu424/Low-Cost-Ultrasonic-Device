32cf4527221f9b5a9c924f3c633c8772279a1605
Vance Gullberg <vgullber@uwo.ca>
2018-11-08 15:19:59 -0500

    new Container class and header

---
+++ LCUD_Project/Container.cpp
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <dirent.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include "Container.h"
+   
+//constructor
+Container::Container(){
+
+}
+
+//destructor
+Container::~Container(){
+}
+
+std::string Container::to_string(){
+}
---
+++ LCUD_Project/Container.h
@@ -0,0 +1,15 @@
+#ifndef CONTAINER_H
+#define CONTAINER_H
+#include <string>
+#include <fstream>
+     
+class Container{
+    private:
+        
+    public:
+        Container();
+        ~Container(){;}
+        std::string to_string();
+        
+};
+#endif
