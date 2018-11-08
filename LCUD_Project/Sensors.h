a9181d0e4c7ea06c89a767ec9bd60f7c82db31ce
Vance Gullberg <vgullber@uwo.ca>
2018-11-08 13:13:17 -0500

    updated sensor class and added main

--- LCUD_Project/Sensors.h
+++ LCUD_Project/Sensors.h
@@ -1,27 +1,25 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 #include <string>
+#include <fstream>
 
 class Sensors{
     private:
-        std::string SensorID,SDuration,STemperature;    //attributes ID, duration, and temperature
+        std::string SensorID, SDuration, STemperature;    //attributes ID, duration, and temperature
+        std::ifstream inFile;    //read from file
         static int NumSensors;    //static class varible NumSensors that keeps track of the total number of sensors 
     public:
         //constructor, destructor, getters, and toString methods
         Sensors();
         ~Sensors();
-        int getNumberOfSensors(){return NumSensors;}
-        std::string getIdentification(){return SensorID;}
+        static int getNumberOfSensors(){return NumSensors;}
+        void updateLine();
+        std::string getSensorID(){return SensorID;}
         std::string getDuration(){return SDuration;}
         std::string getTemperature(){return STemperature;}
         std::string to_string();
-        std::string getSensorID();
-        std::string getTemperature();
-        std::string getDuration();
-        
-
-
 };
 //initialize number of sensors NumSensors to 0
 int Sensors::NumSensors = 0;
 #endif
+
