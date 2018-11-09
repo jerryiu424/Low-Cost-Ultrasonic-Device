1f257a76322f2f9309d0de4e53bf2c23c5eedac8
Vance Gullberg <vgullber@uwo.ca>
2018-11-08 19:25:31 -0500

    update

--- LCUD_Project/Container.cpp
+++ LCUD_Project/Container.cpp
@@ -5,27 +5,32 @@ Container::Container(){
     ID = s.getSensorID();
 }
 
+//get ide
 std::string Container::getID(){
     return ID;
 }
 
-
+//get max volume
 int Container::getMaxVolume(){
     return MaxVolume;
 }
 
+//get shape
 std::string Container::getShapeID(){
     return ShapeID;
 }
 
+//get duration from sensor
 int Container::getDuration(){
     return stoi(s.getDuration());
 }
 
+//get temp from sensor
 int Container::getTemperature(){
     return stoi(s.getTemperature());
 }
 
+//update
 void Container::update(){
     s.updateLine();
 }
