cea1f7655ac0d429364403c7c7b96db30b29839e
Vance Gullberg <vgullber@uwo.ca>
2018-11-26 18:19:37 -0500

    chaged it bacl

--- LCUD_Project/Sensors.cpp
+++ LCUD_Project/Sensors.cpp
@@ -21,41 +21,22 @@ Sensors::~Sensors(){
 
 //read line output by arduino and store info in respective attributes
 void Sensors::update(){
-    inFile.seekg(-1,std::ios_base::end);       //go to the last character in the file         
-    bool boolLoop = true;
-    //keeps looping back, looking at characters until it finds the newline character
-    while(boolLoop){
-        char ch;
-        inFile.get(ch);
-        //if it loops to the very beginning without ever 
-        //seeing a newline character, the last line is the first line
-        if((int)inFile.tellg() <= 1) {             // If the data was at or before the 0th byte
-            inFile.seekg(0);                       // The first line is the last line
-            boolLoop = false;                // So stop there
-        }
-        //if newline found, we've found our last line
-        else if(ch == '\n'){
-            boolLoop = false;
-        }
-        else{
-            inFile.seekg(-2,std::ios_base::cur);
+    std::string line;
+    if (getline(inFile, line)){
+        std::stringstream ss(line);
+        std::string duration;
+        getline(ss,duration, ' ');
+        std::string temperature;
+        getline(ss,temperature,' ');
+
+        //if there is data, set attributes accordingly
+        if(!duration.empty() && !temperature.empty()){
+            SDuration = stof(duration);
+            STemperature = stof(temperature);
+             std::cout << duration; 
+            std::cout << temperature;
         }
     }
-    std::string line;
-    getline(inFile,line);
-    std::stringstream ss(line);
-    std::string duration;
-    getline(ss,duration, ' ');
-    std::string temperature;
-    getline(ss,temperature,' ');
-
-    std::cout << duration; 
-    std::cout << temperature;
-
-    /*
-    SDuration = stof(duration);
-    STemperature = stof(temperature);
-    */
 }
 
 
