bae3f0b309073cd7714b74fad2b24084274ff1ad
Vance Gullberg <vgullber@uwo.ca>
2018-11-26 21:42:24 -0500

    added trend code

--- LCUD_Project/Sensors_exampleMain.cpp
+++ LCUD_Project/Sensors_exampleMain.cpp
@@ -2,6 +2,9 @@
 #define SLEEP 1500000
 using namespace std;
 
+void draw(int []);
+string multiply(int t);
+
 int main(){
     cout << "current number of sensors: " + to_string(Sensors::getNumberOfSensors())<< endl;    //should be 0
     Sensors first;
@@ -11,25 +14,25 @@ int main(){
 
 
     
+    int temp[] ={0,0,0,0,0,0,0,0,0,0};
     int position = 0;
+
     usleep(SLEEP);
     for (int i = 0; i<10;i++){
         first.update();    //read line, which updates duration and temp
         cout << "first sensor has duration: " + to_string(first.getDuration()) + " and temp: " + to_string(first.getTemperature()) << endl;
+        
+        temp[position] = ceil(first.getDuration())/10.0;
+        position++;
+        position = position % 10;
+        draw(temp);
+
         usleep(SLEEP);//pause for 1 second
     }
     
     return 0;
 }
 
-/*
-//print info of first sensor in 1 second increments 10 times
-int temp[] ={0,0,0,0,0,0,0,0,0,0};
-int position = 0;
-temp[position] = ceil(first.getDuration())/10.0;
-position++;
-position = position % 10;
-draw(temp);
 
 void draw(int a[]){
     ofstream newFile;
@@ -48,4 +51,3 @@ string multiply(int t){
     }
     return res;
 }
-*/
