b4f7e550fc257624970c8e2f4df4512a6d916113
Vance Gullberg <vgullber@uwo.ca>
2018-11-26 21:50:30 -0500

    changed drawing func

--- LCUD_Project/Sensors_exampleMain.cpp
+++ LCUD_Project/Sensors_exampleMain.cpp
@@ -12,42 +12,12 @@ int main(){
     cout << "first sensor has ID: " + first.getSensorID() << endl;
     cout << "current number of sensors: " + to_string(Sensors::getNumberOfSensors()) << endl; 
 
-
-    
-    int temp[] ={0,0,0,0,0,0,0,0,0,0};
-    int position = 0;
-
     usleep(SLEEP);
     for (int i = 0; i<10;i++){
         first.update();    //read line, which updates duration and temp
         cout << "first sensor has duration: " + to_string(first.getDuration()) + " and temp: " + to_string(first.getTemperature()) << endl;
-        
-        temp[position] = ceil(first.getDuration())/10.0;
-        position++;
-        position = position % 10;
-        draw(temp);
-
         usleep(SLEEP);//pause for 1 second
     }
     
     return 0;
-}
-
-
-void draw(int a[]){
-    ofstream newFile;
-    newFile.open("drawing.txt");
-    for(int i =0;i<10;i++){
-    
-        newFile <<multiply(a[i])+".\n";
-    }
-    newFile.close();
-
-}
-string multiply(int t){
-    string res = "";
-    for (int i = 0;i<=t;i++){
-        res = res+"o";
-    }
-    return res;
 }
