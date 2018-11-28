3e3aab46009db27632360f7e5656a7431679e6bc
Marco Manuel <mmanuel@uwo.ca>
2018-11-27 23:05:44 -0500

    big changes

--- LCUD_Project/Interface.cpp
+++ LCUD_Project/Interface.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include "Container.h"
+//#include "Container.h"
 #include "Cube.h"
 #include "Cylinder.h"
 #include "Cuboid.h"
-#include "Sensors.h"
+//#include "Sensors.h"
 using namespace std;
 
 void CubeFunc(Cube);
