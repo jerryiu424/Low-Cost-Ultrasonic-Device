8ed334338b9d46130f59cd3641e5aa617a8dd9aa
Joon Lee <jlee3227@uwo.ca>
2018-11-29 18:49:20 -0500

    add comments to Sensors class

--- LCUD_Project/Sensors.h
+++ LCUD_Project/Sensors.h
@@ -27,8 +27,25 @@ class Sensors{
         ~Sensors(){;}   // The destructor method
         void update();  // Function declaration for update()
 
+        /**
+         *  @brief      The getter method for the SensorID.
+         *  @return     the SensorID attribute.
+         */
         std::string getSensorID(){return SensorID;}
-        float getDuration(){return SDuration;}    
+        
+        /**
+         *  @brief      The getter method for the duration.
+         *  @details    This method returns the time it took the ultrasonic wave emmited by the sensor to travel 
+         *              from the sensor and come back.
+         *  @return     the SDuration attribute.
+         */
+        float getDuration(){return SDuration;}
+
+        /**
+         *  @brief      The getter method for the temperature.
+         *  @details    This method returns the temperature of the environment the container is in.
+         *  @return     the STemperature attribute.
+         */
         float getTemperature(){return STemperature;}
         
         std::string to_string();    // Function declaration for the to_string method, which will return a formatted string containing the duration and temperature
