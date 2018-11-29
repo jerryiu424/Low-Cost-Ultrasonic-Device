78e89b741ac3a3f701fd5c3800fcbd30ede77f59
Joon Lee <jlee3227@uwo.ca>
2018-11-29 18:34:40 -0500

    add comments to Sensors class

--- LCUD_Project/Sensors.h
+++ LCUD_Project/Sensors.h
@@ -12,24 +12,25 @@
  *
  *  @details    This class utilizes the sensor connected to the Arduino and stores the values it recieves
  *              into attributes that can be used to calculate the volume in a container.
- *              The attributes of the class are the ID of the sensor, the duration, temperature 
+ *              The attributes of the class are the ID of the sensor, the duration, temperature and a string
+ *              that contains the data from the physical sensor.
  * 
  *  @author    Vance Gullberg
  */
 class Sensors{
     private:
         std::string SensorID;
-        float SDuration, STemperature;    //attributes ID, duration, and temperature
-        static int NumSensors;    //static class varible NumSensors that keeps track of the total number of sensors 
+        float SDuration, STemperature;
         std::string file;
     public:
-        //constructor, destructor, getters, and toString methods
-        Sensors();
-        ~Sensors();
-        void update();
+        Sensors();      // Function declaration for the constructor
+        ~Sensors();     // Function declaration for the destructor
+        void update();  // Function declaration for update()
+
         std::string getSensorID(){return SensorID;}
-        float getDuration(){return SDuration;}
+        float getDuration(){return SDuration;}    
         float getTemperature(){return STemperature;}
-        std::string to_string();
+        
+        std::string to_string();    // Function declaration for the to_string method, which will return a formatted string containing the duration and temperature
 };
 #endif
