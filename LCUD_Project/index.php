911935f1adafba54fca837ec574873ba252a51ef
Joon Lee <jlee3227@uwo.ca>
2018-11-28 23:48:25 -0500

    Jerry's commit night

--- LCUD_Project/index.php
+++ LCUD_Project/index.php
@@ -3,9 +3,16 @@
 <html><head>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="http://code.jquery.com/jquery-latest.js" ></script> 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  
+	<script type="text/javascript" >
+	setInterval("refreshContent();", 5000);
+	function refreshContent() {
+		$('#content').load(location.href + ' #innerContent');
+	}
+	</script>  
   <meta content="text/html;charset=ISO-8859-1" http-equiv="Content-Type">
   <title>LCUD</title>
 
@@ -26,18 +33,22 @@
   <tbody>
     <tr>
       <td style="vertical-align: top; background-color: rgb(153, 255, 255); width: 471px; text-align: center;">
-      <div style="background-color: white;"><span style="font-family: American Typewriter;"><br>
-Sensor Number:</span><br>
+      <div style="background-color: white;"><span style="font-family: American Typewriter;"><br><br>
+Sensor Number: 1</span><br>
       <br>
       <br>
       </div>
       </td>
+      
       <td style="vertical-align: top; text-align: center; background-color: rgb(153, 255, 255); width: 472px;">
-      <div style="text-align: left; background-color: white;"><span style="font-family: American Typewriter;" id="duration">Duration:</span><br>
-      <span style="font-family: American Typewriter;" id="temperature">Temperature:</span><br>
-      <span style="font-family: American Typewriter;" id="distance">Distance:</span><br>
-      <span style="font-family: American Typewriter;" id="volume">Volume:</span><br>
-      <span style="font-family: American Typewriter;"> </span></div>
+      <div style="text-align: left; background-color: white;" id="content">
+		<div id="innerContent" style="span:nth-child(odd) {background: #grey;}">      
+      <?php
+			include "connectdb.php";
+ 			include "getdata.php";
+		?>
+		</div>
+      </div>
       </td>
     </tr>
   </tbody>
@@ -46,9 +57,8 @@ Sensor Number:</span><br>
 </div>
 
 <br>
-<?php
- include "connectdb.php"
-?> 
+
+
 <a href="./form.php"> <button type="button" class="btn btn-primary">Set up</button></a>
 
 
