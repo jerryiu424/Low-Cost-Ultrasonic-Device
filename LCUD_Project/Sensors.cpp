505e5b4b76e570c272c532c24db85cde1d692edc
Joon Lee <jlee3227@uwo.ca>
2018-11-28 21:54:21 -0500

    attempting to fix reading from text file when starting up interface

--- LCUD_Project/Sensors.cpp
+++ LCUD_Project/Sensors.cpp
@@ -9,7 +9,7 @@ Sensors::Sensors(){
     //get number of current sensors
 	int ID = 0; //NumSensors;
     SensorID = std::to_string(ID);    //convert int to string
-    file = "/dev/ttyACM" + SensorID;
+    file = "/dev/ttyACM0"; //+ SensorID;
     //NumSensors++;    //increment total number of sensors by 1
 }
 
