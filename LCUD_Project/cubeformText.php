ca141ad8a32cdf7dc5593f4d89db75d251fbbc3d
Joon Lee <jlee3227@uwo.ca>
2018-11-29 22:14:44 -0500

    comment frontend

--- LCUD_Project/cubeformText.php
+++ LCUD_Project/cubeformText.php
@@ -1,7 +1,15 @@
-<!-- Connects to the database -->
+<!-- POSTs the data from input form to data.txt file for program use -->
 <?php
- include "connectdb.php"
+		error_reporting(E_ALL);
+		ini_set('display_errors',1);
+		$data = $_POST['side'] . "\n";
+		$data = "CUBE\n" . $data;
+		$fp = file_put_contents("/home/pi/Desktop/group10/LCUD_Project/data.txt", $data, LOCK_EX);
+		if($fp === false){
+			die('Unable to open file');
+		}
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -11,36 +19,14 @@
   	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  		
 	<title>Set Up form</title>
 </head>
-<!-- Creates a dropdown menu for creating a Container, redirects to relevant page for next input -->
+<!-- Confirmation page for cube input -->
 <body style="color: black; background-color: rgb(229, 229, 229); background-image: url(images/bg.jpg);" alink="#000099" link="#000099" vlink="#a700a7">
+	<h1>Set up COMPLETE!</h1>
 	<br><br><br><br>
-	<div class="container">
-  		<div class="row">
-  		<p>Select a shape: <br></p>
-    		<div class="col-lg">
-				<div class="dropdown">
-  					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					----------    		
-  					</button>
-					<!-- Drop down options -->
-  					<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
- 						<a class="dropdown-item" href="./cubeform.php">Cube</a>
-  						<a class="dropdown-item" href="./cuboidform.php">Cuboid</a>
-   					<a class="dropdown-item" href="./cylinderform.php">Cylinder</a>
-  					</div>
-				</div>
-			</div>
-		</div>
-		<br><br><br><br>
-		<div class="row">
-			<div class = "col-lg">
-				<!-- Return to index.php -->
-				<a href="./index.php"> <button type="button" class="btn btn-primary">Go Back</button></a>
-			</div>		
-		</div>
-	</div>
+	<!-- Return to index.php -->
+	<p><a href="./index.php"> <button type="button" class="btn btn-primary">Go Back</button></a></p>
 </body>
 </html>
--- LCUD_Project/getdata.php
+++ LCUD_Project/getdata.php
@@ -1,5 +1,5 @@
 <?php
-
+// Queries the databse for the latest entry
 $query = "SELECT * FROM sensor WHERE datetime = (SELECT MAX(datetime) FROM sensor)";
 
 $result = mysqli_query($connection, $query);
@@ -8,12 +8,16 @@ if(!$query){
 	die("database query failed");
 }
 else {
+	// Fetches the row for the result
 	$row = mysqli_fetch_assoc($result);
-	
+	// Each line echo's an associated data value form the database
 	echo "<span style=\"font-family: American Typewriter;\"> <b>Duration: </b>" . $row['duration'] . " ms</span><br>";
 	echo "<span style=\"font-family: American Typewriter;\"> <b>Temperature: </b>" . $row['temperature'] . " &#176;C</span><br>";
 	echo "<span style=\"font-family: American Typewriter;\"> <b>Distance: </b>" . $row['distance'] . " cm</span><br>";
 	echo "<span style=\"font-family: American Typewriter;\"> <b>Volume: </b>" . $row['volume'] . " mL</span><br>";
+	echo "<span style=\"font-family: American Typewriter;\"> <b>Current Level: </b>" . $row['level'] . " cm</span><br>";
+	echo "<span style=\"font-family: American Typewriter;\"> <b>Percent Full: </b><span id = \"percent\">" . $row['percentfull'] . "</span> %</span><br>";
+	echo "<span style=\"font-family: American Typewriter;\"> <b>Shape: </b>" . $row['shape'] . "</span><br>";
 	echo "<span style=\"font-family: American Typewriter;\"> <b>Date and Time: </b>" . $row['datetime'] . "</span><br>";
 }
 ?>
