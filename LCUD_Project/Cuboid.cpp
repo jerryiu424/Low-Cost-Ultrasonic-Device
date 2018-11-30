5300b13a2f164f4889b027ed099ba3b7c5ec051b
Vance Gullberg <vgullber@uwo.ca>
2018-11-29 20:07:13 -0500

    you, you got what i need

--- LCUD_Project/Cuboid.cpp
+++ LCUD_Project/Cuboid.cpp
@@ -1,6 +1,21 @@
+/**
+ * @brief Cuboid class methods
+ * 
+ * @details
+ *          This file implements the cuboid class member methods;
+ *          It consists of the constructor, and a toString method
+ * 
+ * @author Reid Jackson
+ */
 #include "Cuboid.h"
    
-//constructor
+/**
+ * @brief cuboid constructor
+ * 
+ * @details
+ *          calculates and sets the max volume, shapeid, and width, length, and height
+ * 
+ */
 Cuboid::Cuboid(float width,float length,float height){
     Cwidth = width;
     Clength = length;
@@ -10,6 +25,15 @@ Cuboid::Cuboid(float width,float length,float height){
     ShapeID = "Cuboid";
 }
 
+/**
+ * @brief cube toString method
+ * 
+ * @details
+ *          returns string of maxVolume and side
+ *          
+ * @return returns string of data
+ * 
+ */
 std::string Cuboid::to_string(){
     return "Type Cuboid with volume: " + std::to_string(MaxVolume);
 }
