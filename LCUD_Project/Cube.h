50fe073f3f5286ee378d9a3f6c55b98f71d651a8
Joon Lee <jlee3227@uwo.ca>
2018-11-29 19:39:11 -0500

    add comments to Cube class

--- LCUD_Project/Cube.h
+++ LCUD_Project/Cube.h
@@ -2,14 +2,28 @@
 #define CUBE_H
 #include "Container.h" 
 
+/** 
+ *  @brief      The Cube class.
+ *
+ *  @details    This class is a child class of the Container class. Since the shape of this Container is a cube
+ *              the only measurement we require is side length. From this we can determine the max possible volume
+ *              of the container.
+ * 
+ *  @author     Vance Gullberg
+ */ 
 class Cube : public Container{
-    //attribute side
     private:
         float side;
     public:
-        Cube(float s);
-        ~Cube(){;}
-        std::string to_string();
-        float getCubeSide();
+        Cube(float s);  // The function declaration of the constructor
+        ~Cube(){;}      // The destructor method
+
+        std::string to_string();            // Function declaration for the to_string method, which will return a formatted string containing the volume and side lengths 
+
+        /** 
+         *  @brief      The getter method for the side length of the cube..
+         *  @return     the side attribute.
+         */
+        float getCubeSide(){return side;}
 };
 #endif
